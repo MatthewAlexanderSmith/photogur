194a1259ee46031d380ca71f3d3e1dd7f6656630
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-01-25 14:00:58 -0500

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,7 @@
 class PicturesController < ApplicationController
   def index
+    #Instance variables defined in the controller are
+    #automatically available in the view.
+    @pictures = Picture.all
   end
 end
