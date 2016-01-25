f109f73a0e32efea9e447565852b72db82cdf4ff
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-01-25 18:46:39 -0500

    Refactoring to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
 
   #What does 'pictures' before hash rocket do?
-
-  get 'pictures' => 'pictures#index'
-
   #Two routes required for creating new resource
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
-  patch 'pictures/:id' => 'pictures#update'
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
   root 'pictures#index'
+
+  resources :pictures
+
+  # get 'pictures' => 'pictures#index'
+  # post 'pictures' => 'pictures#create'
+  # get 'pictures/new' => 'pictures#new'
+  #
+  # get 'pictures/:id' => 'pictures#show', as: 'picture'
+  #
+  # get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
+  # patch 'pictures/:id' => 'pictures#update'
+  #
+  # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
